0a5f0d1ff439d031376d40d6ec05c3db5192e660
almeida-matheus <matheusalmeidacosta1212@gmail.com>
2023-03-25 12:31:42 -0300

    handle throttling exception

--- resources/aws.py
+++ resources/aws.py
@@ -1,9 +1,10 @@
 import boto3
+from botocore.exceptions import ClientError
+import time
 
 class AWS:
 
     def __init__(self, profile=None):
-
         self.session = boto3.Session()
         if profile:
             self.session = boto3.Session(profile_name=profile)
@@ -35,17 +36,27 @@ class AWS:
                 all_roles.append(user)
         return all_roles
 
-    def simulate_policy(self, arn, actions, resources, match_all_actions):
-        response = self.iam_client.simulate_principal_policy(
-            PolicySourceArn=arn,
-            ActionNames=actions,
-            ResourceArns=resources
-        )
-
-        #? simple report - check if the principal have allowed some actions in the specific resource
-        if match_all_actions: #? must contain all specified actions
-            has_any_action_allowed = not(any([(r['EvalDecision'] == 'implicitDeny' or r['EvalDecision'] == 'implicitDeny') for r in response['EvaluationResults']]))
+    def simulate_policy(self, arn, actions, resources, match_all_actions, tries=1):
+        try:
+            response = self.iam_client.simulate_principal_policy(
+                PolicySourceArn=arn,
+                ActionNames=actions,
+                ResourceArns=resources
+            )
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ThrottlingException':
+                if tries <= 3:
+                    print('throttling exception occured. retrying attempt no.: {}'.format(str(tries)))
+                    time.sleep(3*tries)
+                    return self.simulate_policy(arn, actions, resources, match_all_actions, tries+1)
+                else:
+                    print('attempted 3 times but no success. raising throttling exception')
+                    raise e
         else:
-            has_any_action_allowed = any([(r['EvalDecision'] == 'allowed') for r in response['EvaluationResults']])
-            # has_any_action_allowed = any([(r['EvalResourceDecision'] == 'allowed') for r in response['EvaluationResults']['ResourceSpecificResults']])
-        return has_any_action_allowed
+            #? simple report - check if the principal have allowed some actions in the specific resource
+            if match_all_actions: #? must contain all specified actions
+                has_any_action_allowed = not(any([(r['EvalDecision'] == 'implicitDeny' or r['EvalDecision'] == 'implicitDeny') for r in response['EvaluationResults']]))
+            else:
+                has_any_action_allowed = any([(r['EvalDecision'] == 'allowed') for r in response['EvaluationResults']])
+                # has_any_action_allowed = any([(r['EvalResourceDecision'] == 'allowed') for r in response['EvaluationResults']['ResourceSpecificResults']])
+            return has_any_action_allowed
